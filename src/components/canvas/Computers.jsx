c7928aec0e78cdd1864854f765c74254d561ec6b
AvionShea <avionsheadesigns@gmail.com>
2023-07-16 00:10:38 -0400

    hero.jsx, computers.jsx, and Loader.jsx changes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,12 +10,20 @@ const Computers = () => {
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
-      <pointLight intensity={1}/>
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.2}
+        penumbra={1}
+        intensity={3}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={0.5}/>
       <primitive 
       object={computer.scene}
       scale={0.75}
-      position={[0, -3.25, -1.5]}
-      rotation={[-0.01, -0.2, -0.1]}
+      position={[0, -3.50, -1.5]}
+      rotation={[-0.001, -0.2, -0.001]}
       />
     </mesh>
   )

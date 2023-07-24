535f0366e8b9217ab55cc687cf9e67e68b05b53b
AvionShea <avionsheadesigns@gmail.com>
2023-07-23 20:32:39 -0400

    changes to about.jsx and index.js

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,12 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ServiceCard = ({title, Icon, index}) => {
+  return (
+    <p>{title}</p>
+  )
+}
+
 const About = () => {
   return (
     <>
@@ -17,6 +23,12 @@ const About = () => {
     className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
     Innovative full-stack software engineer with experience in HTML/CSS and JavaScript. I have experience in Oracle, MySQL, Python, Node.js, React, and Three.js. I'm a quick learner and collaborate closely with other developers to create efficient, scalable, and user-friendly solutions that solve real-world problems. See below on how to contact me!
     </motion.p>
+    <div className="mt-20 flex flex-wrap gap-10">
+      { services.map((service, index) => (
+        <ServiceCard key={service.title} index={index} {...service} />
+      )) }
+
+    </div>
     </>
   )
 }

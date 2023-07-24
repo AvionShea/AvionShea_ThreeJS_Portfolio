f75fe50bc643d29135a5d5bac4969bbf6de96365
AvionShea <avionsheadesigns@gmail.com>
2023-07-23 20:36:10 -0400

    changed wording in about.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
     </motion.div>
     <motion.p variants={fadeIn("", "", 0.1, 1)}
     className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
-    Innovative full-stack software engineer with experience in HTML/CSS and JavaScript. I have experience in Oracle, MySQL, Python, Node.js, React, and Three.js. I'm a quick learner and collaborate closely with other developers to create efficient, scalable, and user-friendly solutions that solve real-world problems. See below on how to contact me!
+    Innovative full-stack software engineer with experience in HTML/CSS and JavaScript. Knowledgeable of Oracle, MySQL, Python, Node.js, React, and Three.js. A quick learner that collaborates closely with other developers to create efficient, scalable, and user-friendly solutions that solve real-world problems. See below on how to contact me!
     </motion.p>
     <div className="mt-20 flex flex-wrap gap-10">
       { services.map((service, index) => (

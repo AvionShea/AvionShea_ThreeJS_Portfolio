0a842552fdcc510cbaba4579d303f94459e2c94d
AvionShea <avionsheadesigns@gmail.com>
2023-07-23 20:22:40 -0400

    periodic save

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-import Tilt from "react-tilt";
+import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
-import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
   return (
     <>
-    <motion.div>
+    <motion.div variants={textVariant()}>
       <p className={styles.sectionSubText}>Introduction</p>
-      <h2 className={styles.sectionHeadText}>Overview</h2>
+      <h2 className={styles.sectionHeadText}>Overview.</h2>
     </motion.div>
+    <motion.p variants={fadeIn("", "", 0.1, 1)}
+    className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+    Innovative full-stack software engineer with experience in HTML/CSS and JavaScript. I have experience in Oracle, MySQL, Python, Node.js, React, and Three.js. I'm a quick learner and collaborate closely with other developers to create efficient, scalable, and user-friendly solutions that solve real-world problems. See below on how to contact me!
+    </motion.p>
     </>
   )
 }

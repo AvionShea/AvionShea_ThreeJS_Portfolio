8d4ffe7c8c1d745ca9f9725beaaf0ebdbde0bd3e
AvionShea <avionsheadesigns@gmail.com>
2023-08-16 18:30:52 -0400

    completed ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -9,8 +9,24 @@ import CanvasLoader from '../Loader'
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl])
   return (
-    <Float>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25}/>
+      <directionalLight position={[0, 0, 0.05]}/>
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1,3]}/>
+        <meshStandardMaterial
+        color="#63C5DA"
+        polygonOffset
+        polygonOffsetFactor={-5}
+        flatShading
+        />
+        <Decal
+        position={[0, 0, 1]}
+        rotation={[ 2 * Math.PI, 0, 6.25]}
+        flatShading
+        map={decal}
+        />
+      </mesh>
     </Float>
   )
 }

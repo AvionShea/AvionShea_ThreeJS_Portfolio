e8b426dc204fabe02c488e3a4a557feb3de8ee39
AvionShea <avionsheadesigns@gmail.com>
2023-08-19 01:29:01 -0400

    changes made to works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,6 +7,21 @@ import { SectionWrapper} from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ProjectCard = ({ index, name, description, tags, image, source_code_link}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt options={{max: 45, scale:1, speed:450}} className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+        <div className="relative w-full h-[230px]">
+          <img src={image} alt={name} className="w-ful h-full object-cover rounded-2xl"/>
+          <div>
+            
+          </div>
+        </div>
+
+      </Tilt>
+    </motion.div>
+  )
+}
 
 const Works = () => {
   return (
@@ -15,6 +30,21 @@ const Works = () => {
       <p className={styles.sectionSubText}>My Projects</p>
       <h2 className={styles.sectionHeadText}>Projects.</h2>
     </motion.div>
+
+    <div className="w-full flex">
+      <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
+        The following projects showcase my skills and experience through real-world examples of my work. Each project is briefly describe with links to code repositories and live demos. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.
+      </motion.p>
+    </div>
+
+    <div className="mt-20 flex flex-wrap gap-7">
+    {projects.map((project, index) => (
+      <ProjectCard key={`project-${index}`}
+      index={index} 
+      {...project}
+      />
+    ))}
+    </div>
     </>
   )
 }

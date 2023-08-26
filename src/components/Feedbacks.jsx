b29f869c369653ede1d45508055558acf50b5ab9
AvionShea <avionsheadesigns@gmail.com>
2023-08-26 00:56:42 -0400

    feedbacks.jsx changes

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -6,7 +6,7 @@ import { fadeIn, textVariant } from '../utils/motion'
 import { testimonials } from '../constants'
 
 const FeedbackCard = ({index, testimonial, name, designation, company, image}) => (
-  <motion.div>
+  <motion.div variants={fadeIn("", "spring", index*0.5, 0.75)} className="bg-black-200 p-10 rounded-3xl xs:w-[300px] w-full">
     <p>{testimonial}</p>
   </motion.div>
 )

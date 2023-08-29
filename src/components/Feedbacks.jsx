dc87cbe1a2f90c03e9f08dce49648b5f586c69f5
AvionShea <avionsheadesigns@gmail.com>
2023-08-28 21:27:14 -0400

    changes to feedbacks.jsx

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -8,7 +8,18 @@ import { testimonials } from '../constants'
 const FeedbackCard = ({index, testimonial, name, designation, company, image}) => (
   <motion.div variants={fadeIn("", "spring", index*0.5, 0.75)} className="bg-black-200 p-10 rounded-3xl xs:w-[300px] w-full">
     <p className="text-white font-black text-[48px]">"</p>
+
+  <div className='mt-1'>
     <p>{testimonial}</p>
+    <div className='mt-7 flex justify-between items-center gap-1'>
+      <div className='flex-1 flex flex-col'>
+        <p>
+          <span>@</span> {name}
+        </p>
+      </div>
+    </div>
+  </div>
+
   </motion.div>
 )
 
